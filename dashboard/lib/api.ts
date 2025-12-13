317ba29ecfb68bdb757fbb9b33bfbe1f649d1a1f
Pranjal Shahi <prabhakar.shahi1@gmail.com>
2025-12-13 14:01:44 +0530

    Update api.ts

--- dashboard/lib/api.ts
+++ dashboard/lib/api.ts
@@ -1,4 +1,4 @@
-const BACKEND_URL = "http://127.0.0.1:8000";
+const BACKEND_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 export async function inferIncident(videoFile: File) {
   const formData = new FormData();
